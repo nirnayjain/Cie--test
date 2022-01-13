36fe10077f4a234f0412e7f0229302677cc25953
Nirnay Jain <nirujainofficial@gmail.com>
2022-01-14 02:21:06 +0530

    Url updated

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-//export const url="https://cie-admin-gov.herokuapp.com";
-export const url="https://admin.cie.telangana.gov.in";
+export const url="https://cie-admin-gov.herokuapp.com";
+//export const url="https://admin.cie.telangana.gov.in";
 //export const url="http://localhost:3001";
