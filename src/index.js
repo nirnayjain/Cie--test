a1683693283b8fb159882a0529ff0162f75c5a22
Nirnay Jain <nirujainofficial@gmail.com>
2022-01-14 13:46:32 +0530

    Live recapcha ke added

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { BrowserRouter } from "react-router-dom";
 
 const setupAxios = () => {
   axios.defaults.baseURL =
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:5000/":
-      "https://api.cie.telangana.gov.in/";
     // process.env.NODE_ENV === "development"
-    // ? "http://localhost:5000/":
-    //  "https://cie-api-gov.herokuapp.com/";
+    //   ? "http://localhost:5000/":
+    //   "https://api.cie.telangana.gov.in/";
+    process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/":
+     "https://cie-api-gov.herokuapp.com/";
      axios.defaults.headers = {
       'Cache-Control': 'no-cache',
       'Pragma': 'no-cache',
